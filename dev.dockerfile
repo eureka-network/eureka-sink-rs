a9437af8f8533aa97689ef705d8fc5d7b7aba6fe
jorgeantonio21 <matroid@outlook.com>
2023-02-27 16:22:59 +0000

    add Dockerfile

--- dev.dockerfile
+++
@@ -1,8 +0,0 @@
-FROM rust:latest
-RUN apt update
-RUN apt install -y libpq-dev
-RUN cargo install diesel_cli --no-default-features --features postgres
-WORKDIR /eureka-substreams-sink
-COPY . .
-RUN cargo install --path .
-CMD bash -c "diesel setup"
--- tests/dbloader/src/lib.rs
+++ tests/dbloader/src/lib.rs
@@ -1,11 +1,13 @@
-use std::env;
+use std::{env, path::PathBuf};
 
 use eureka_sink_postgres::db_loader::Loader;
 
 #[test]
 fn it_works_start_db_loader() {
-    let database_url = env::var("DATABASE_URL").unwrap();
-    let database_url = std::path::PathBuf::try_from(database_url).unwrap();
+    const DATABASE_URL: &str = "postgres://dev-node:insecure-change-me-in-prod@postgres:5432/dev-node?sslmode=disable";
+    let database_url = PathBuf::try_from(DATABASE_URL).unwrap();
     let schema = String::from("my_schema");
     let loader = Loader::new(database_url, schema).unwrap();
+
+    let schema_file = PathBuf::try_from("../sql/schema.sql").unwrap();
 }
