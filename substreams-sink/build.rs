0f3b530182cc5ac91ae383a86bcb6f7ee9ab1a47
benjaminbollen <ben@doru.io>
2023-03-02 16:20:11 +0000

    download proto files in build.rs

--- substreams-sink/build.rs
+++ substreams-sink/build.rs
@@ -1,4 +1,4 @@
-// credit to https://gist.github.com/danburkert/02f8010714a8f86cabcb854a5a2d9e09
+// credit to and adapted from https://gist.github.com/danburkert/02f8010714a8f86cabcb854a5a2d9e09
 extern crate curl;
 extern crate prost_build;
 extern crate tempdir;
@@ -10,49 +10,37 @@ use std::io;
 use std::io::{Write, ErrorKind};
 use std::path::{Path, PathBuf};
 
-// use tempdir::TempDir;
+use curl::easy::Easy;
 
 const TAG: &'static str = "v0.2.0";
 const URL_BASE: &'static str = "https://raw.githubusercontent.com/streamingfast/substreams";
 
+const PROTO_IMPORT: &'static str = "proto/import-substreams";
 const PROTOS: &[&str] = &[
-    "proto/sf/substreams/v1/substreams.proto",
-    "proto/sf/substreams/v1/package.proto",
+    "sf/substreams/v1/substreams.proto",
+    "sf/substreams/v1/package.proto",
+    // imported dependencies
+    "sf/substreams/v1/modules.proto",
+    "sf/substreams/v1/modules.proto",
+    "sf/substreams/v1/clock.proto",
+    "google/protobuf/descriptor.proto",
+    "google/protobuf/any.proto",
+    "google/protobuf/timestamp.proto",
 ];
 
 fn main() {
-    // let kudu_home = match env::var("KUDU_HOME") {
-    //     Ok(kudu_home) => PathBuf::from(kudu_home),
-    //     Err(_) => {
-    //         let kudu_home =
-    //             PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
-    //                     .join(format!("apache-kudu-{}", VERSION));
-
-    //         if !kudu_home.exists() {
-    //             download_protos(&kudu_home);
-    //         }
-    //         kudu_home
-    //     },
-    // };
-
     let root_path = match get_project_root() {
         Ok(path) => path,
         Err(e) => panic!("Could not get project root: {}", e),
     };
 
-    optional_download_proto_if_not_exist(&root_path);
+    optional_download_protos_if_not_exist(&root_path);
 
-    // download_protofiles(&root_path);
+    let imported_protos = PROTOS.iter()
+                       .map(|proto| root_path.join(PROTO_IMPORT).join(proto))
+                       .collect::<Vec<_>>();
 
-    println!("root path: {}", root_path.display());
-
-    // root_path.join(format!("substreams-{}", TAG));
-
-    // let protos = PROTOS.iter()
-    //                    .map(|proto| kudu_home.join("src").join("kudu").join(proto))
-    //                    .collect::<Vec<_>>();
-
-    // prost_build::compile_protos(&protos, &[kudu_home.join("src")]).unwrap();
+    // prost_build::compile_protos(&imported_protos, "TODO: INCLUDES NOW OK?"").unwrap();
 }
 
 // return the path of the root of the current repository
@@ -76,57 +64,47 @@ fn get_project_root() -> io::Result<PathBuf> {
 // if the download directory for substream proto files does not
 // yet exist, then create the dir and download the proto files;
 // otherwise do no-op
-fn optional_download_proto_if_not_exist(root_path: &Path) {
+fn optional_download_protos_if_not_exist(root_path: &Path) {
     let mut path = root_path.to_owned();
-    path.push("proto/import-substreams");
+    path.push(PROTO_IMPORT); // "proto/import-substreams"
     fs::create_dir_all(&path).unwrap();
     
+    for proto in PROTOS {
+        let proto_path = path.join(proto);
+        if !proto_path.exists() {
+            download_proto(proto, &proto_path);
+        } else {
+            println!("proto file already exists or could not access: {}",
+                proto_path.display());
+        }
+    }
 }
 
-// downloads proto file from github into a path
-// fn download_protofiles(target: &Path) {
-//     // let tempdir = TempDir::new_in(target.parent().unwrap(), "proto_download").unwrap();
-//     let mut path = target.to_owned();
-//     path.push("proto2");
-//     fs::create_dir(&path).unwrap();
-//     path.push("download-test");
-//     fs::create_dir(&path).unwrap();
-// }
+// download the proto file from github
+// todo: is it better to download files first in a temp directory
+//       and then move them to the target directory?
+fn download_proto(proto: &&str, target: &Path) {
+    let mut data = Vec::new();
+    let mut handle = Easy::new();
 
-// fn download_protos(target: &Path) {
-//     let tempdir = TempDir::new_in(target.parent().unwrap(), "proto-download").unwrap();
-//     let mut path = tempdir.path().to_owned();
-//     path.push("src");
-//     fs::create_dir(&path).unwrap();
-//     path.push("kudu");
-//     fs::create_dir(&path).unwrap();
+    handle.url(&format!("{url_base}/{tag}/{proto}",
+        url_base=URL_BASE,
+        tag=TAG,
+        proto=proto))
+        .expect("failed to configure github URL");
+    handle.follow_location(true)
+        .expect("failed to configure follow location");
 
-//     let mut data = Vec::new();
-//     for proto in PROTOS {
-//         data.clear();
-//         let proto_path = path.join(proto);
-//         fs::create_dir_all(proto_path.parent().unwrap()).unwrap();
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|new_data| {
+            data.extend_from_slice(new_data);
+            Ok(new_data.len())
+        }).expect("failed to write download data");
+        transfer.perform().expect("failed to download proto file from github");
+    }
 
-//         let mut handle = Easy::new();
+    fs::create_dir_all(target.parent().unwrap()).unwrap();
+    fs::File::create(target).unwrap().write_all(&data).unwrap();
 
-//         handle.url(&format!("{url_base}/{version}/src/kudu/{proto}",
-//                             url_base=URL_BASE,
-//                             version=VERSION,
-//                             proto=proto))
-//               .expect("failed to configure Kudu URL");
-//         handle.follow_location(true)
-//               .expect("failed to configure follow location");
-//         {
-//             let mut transfer = handle.transfer();
-//             transfer.write_function(|new_data| {
-//                 data.extend_from_slice(new_data);
-//                 Ok(new_data.len())
-//             }).expect("failed to write download data");
-//             transfer.perform().expect("failed to download Kudu source tarball");
-//         }
-
-//         fs::File::create(proto_path).unwrap().write_all(&data).unwrap();
-//     }
-
-//     fs::rename(&tempdir.into_path(), target).expect("unable to move temporary directory");
-// }
+}
