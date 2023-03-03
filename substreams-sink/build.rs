9124be57973c945d9494b55a72dd1d01acf0feb4
Vadim Frolov <vadim@teza.ai>
2023-03-03 08:42:11 +0100

    replaced submodules with git-download

--- substreams-sink/build.rs
+++ substreams-sink/build.rs
@@ -1,12 +1,32 @@
+const PROTO_SRC: &'static str = "proto";
+const PROTO_DST: &'static str = "proto/imported";
+const PROTOS: &[&str] = &[
+    "sf/substreams/v1/substreams.proto",
+    "sf/substreams/v1/package.proto",
+    "sf/substreams/v1/modules.proto",
+    "sf/substreams/v1/clock.proto",
+];
+
 fn main() {
+    let mut downloader =
+        git_download::repo("https://github.com/streamingfast/substreams").branch_name("v0.2.0");
+
+    for proto in PROTOS {
+        downloader = downloader.add_file(
+            format!("{}/{}", PROTO_SRC, proto),
+            format!("{}/{}", PROTO_DST, proto),
+        );
+    }
+    downloader.exec().unwrap();
+
     tonic_build::configure()
         .build_client(true)
         .compile(
-            &[
-                "substreams/proto/sf/substreams/v1/substreams.proto",
-                "substreams/proto/sf/substreams/v1/package.proto",
-            ],
-            &["substreams/proto"],
+            &PROTOS
+                .iter()
+                .map(|proto| format!("{}/{}", PROTO_DST, proto))
+                .collect::<Vec<_>>(),
+            &[PROTO_DST],
         )
         .unwrap();
 }
