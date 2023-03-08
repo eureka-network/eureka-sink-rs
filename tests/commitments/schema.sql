00844d745b14cb3259ac41473d013e1f3ea13525
benjaminbollen <ben@doru.io>
2023-03-08 17:38:04 +0000

    initial doodles for event commitments

---
+++ tests/commitments/schema.sql
@@ -0,0 +1,15 @@
+create table if not exists ethereummainnet.eth_events (
+    id          text not null constraint eth_events_pk primary key,
+    blocknumber integer,
+    blockhash   text,
+    txhash      text,
+    txindex     integer,
+    logindex    integer,
+    address     text,
+    topic0      text,
+    topic1      text,
+    topic2      text,
+    topic3      text,
+    topic4      text,
+    data        text
+);
